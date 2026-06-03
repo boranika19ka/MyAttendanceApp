8f072a49fe967983b34eecce012284c24aa73f9a
boranika19ka <boranikalong@gmail.com>
2026-06-03 16:34:05 +0700

    repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+repositories {
+    maven { url = uri("https://jitpack.io") }
+}
